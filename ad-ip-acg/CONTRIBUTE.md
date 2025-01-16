ac3778d8a949dda5c8b48bb35e4fa887be638225
Thom Rommens <thomrommens@icloud.com>
2025-01-16 09:12:30 +0100

    Proceed outline 2

---
+++ ad-ip-acg/CONTRIBUTE.md
@@ -0,0 +1,2 @@
+- apply different set of IP ACGs to a different directory 
+(bow, all directories get the same IP ACGs applied).
--- ad-ip-acg/src/ip-acg/main.py
+++ ad-ip-acg/src/ip-acg/main.py
@@ -1,25 +1,90 @@
-# imports
+import boto3
+import logging
 
 
-# constants
+CONFIG_FILE = ""
+
+workspaces = boto3.client("workspaces", region_name="eu-west-1")
 
 
-# module-config
-## set logger to prepend icons
+class DepthFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+
+    def format(self, record):
+        pipes = '|' * getattr(record, 'depth', 0)
+        record.msg = f'{pipes} {record.msg}'
+        return super().format(record)
+
+def setup_logger(name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+
+    formatter = DepthFormatter(
+        '[%(asctime)s] [%(levelname)s] - %(message)s', "%Y-%m-%d %H:%M:%S"
+    )
+    ch.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(ch)
+
+    return logger
 
 
-# exception classes
+class NoTargetDirectoryFoundException(Exception):
+    pass
 
 
-# dataclasses
+class RuleSetEmptyException(Exception):
+    pass
 
 
+class RuleSetTooManyEntriesException(Exception):
+    pass
+
+
+class IpAcgNameTooLongException(Exception):
+    pass
+
+
+
+
+# main logic
+## validators
+## workflow components
+
+
+# workflow
 
 
 def main():
+
+    # Log
+    logger = setup_logger('my_logger', logging.DEBUG)
+    logger.debug('This is level 0 log', extra={'depth': 0})
+    logger.info('This is level 1 log', extra={'depth': 1})
+
+
     # Workflow
-    # get user defined arguments from click 
-    # https://click.palletsprojects.com/en/stable/
+    # - Get user defined arguments from click 
+    #   - https://click.palletsprojects.com/en/stable/
+    #   - specified route (if not recognized, raise error from click)
+
+    # - If/else for route create/modify/delete
+
+    # CREATE
+    # - get input: workspace dirs specified?
+    #   - yes: use these
+    #   - no: get dirs
+
+
+    # TODO if modify route, apply tag ModifiedBy
+    # TODO add dryrun (describe)?
+    # TODO: consistent naming 'rule' vs ip address
+
 
     pass
 
